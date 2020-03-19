471a659f87943a4f875c45c37383d3ed5a2c0cc3
Reinoptland <reinoptland@gmail.com>
2020-03-19 09:35:17 +0100

    Add more specific error handling to auth middleware

--- auth/middleware.js
+++ auth/middleware.js
@@ -5,25 +5,43 @@ async function auth(req, res, next) {
   const auth =
     req.headers.authorization && req.headers.authorization.split(" ");
 
-  if (auth && auth[0] === "Bearer" && auth[1]) {
-    try {
-      const data = toData(auth[1]);
-      const user = await User.findByPk(data.userId);
-      if (!user) {
-        return next("User does not exist");
-      } else {
-        req.user = user;
-        next();
-      }
-    } catch (error) {
-      res.status(400).send({
-        message: `Error ${error.name}: ${error.message}`
-      });
-    }
-  } else {
+  if (!auth || !auth[0] === "Bearer" || !auth[1]) {
     res.status(401).send({
-      message: "Please supply some valid credentials"
+      message:
+        "This endpoint requires an Authorization header with a valid token"
     });
+  }
+
+  try {
+    const data = toData(auth[1]);
+    const user = await User.findByPk(data.userId);
+    if (!user) {
+      return res.status(404).send({ message: "User does not exist" });
+    }
+
+    // add user object to request
+    req.user = user;
+    // next handler
+    return next();
+  } catch (error) {
+    console.log("ERROR IN AUTH MIDDLEWARE", error);
+
+    switch (error.name) {
+      case "TokenExpiredError":
+        return res
+          .status(401)
+          .send({ error: error.name, message: error.message });
+
+      case "JsonWebTokenError":
+        return res
+          .status(400)
+          .send({ error: error.name, message: error.message });
+
+      default:
+        return res.status(400).send({
+          message: "Something went wrong, sorry"
+        });
+    }
   }
 }
 
