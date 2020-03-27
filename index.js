a3cd29815b092e472e69146c3106d05ae181f629
Reinoptland <reinoptland@gmail.com>
2020-03-27 12:41:00 +0100

    Seed using model add delay to requests

--- index.js
+++ index.js
@@ -65,6 +65,26 @@ app.use(corsMiddleWare());
 
 /**
  *
+ * delay middleware
+ *
+ * Since our api and client run on the same machine in development mode
+ * the request come in within milliseconds
+ * To simulate normal network traffic this simple middleware delays
+ * the incoming requests by 1500 second
+ * This allows you to practice with showing loading spinners in the client
+ *
+ * - it's only used when you use npm run dev to start your app
+ * - the delay time can be configured in the package.json
+ */
+
+if (process.env.DELAY) {
+  app.use((req, res, next) => {
+    setTimeout(() => next(), parseInt(process.env.DELAY));
+  });
+}
+
+/**
+ *
  * authMiddleware:
  *
  * When a token is provided:
