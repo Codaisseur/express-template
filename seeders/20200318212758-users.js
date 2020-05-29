3d2892be1934d3b504a386029c038627a34631bd
Reinoptland <reinoptland@gmail.com>
2020-05-29 09:27:17 +0200

    Use bulkinsert for now

--- seeders/20200318212758-users.js
+++ seeders/20200318212758-users.js
@@ -11,54 +11,29 @@ const User = require("../models").user;
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * using User.upsert & Promise.all to create users
-     *
-     * why:
-     *
-     * - allows us to run "npx sequelize-cli db:seed:all" multiple times
-     * - without having to drop the database
-     * - it also allows us to harcode the email and id we want
-     * - we can now use these ids to seed related data in other seed files
-     *
-     * how .upsert works
-     * - if a user with this email and id doesn't exist: create it
-     * - if a user with this email and id does exist: update that user
-     * - since we want to create multiple users this way we use Promise.all
-     *
-     * if you still get a: "ERROR: Validation error", while running this seed
-     * - you might have a user with a different id that you're trying to give the same email
-     * - you might have a user with a different email that you're trying to assign the same id
-     *
-     * how to solve the above error:
-     *
-     * - make sure the combination of id and email in this file are unique
-     * - get rid of any duplicates by dropping the db, creating it again, migrating and seeding
-     *
-     * You can do this using:
-     * npx sequelize-cli db:drop
-     * npm run initdev (creates db, migrates & seeds)
-     *
-     */
-    const users = await Promise.all([
-      User.upsert({
-        id: 1,
-        name: "testuser",
-        email: "test@test.com",
-        password: bcrypt.hashSync("test1234", SALT_ROUNDS)
-      }),
-      User.upsert({
-        id: 2,
-        name: "dummy",
-        email: "dummy@dummy.com",
-        password: bcrypt.hashSync("dummy1234", SALT_ROUNDS)
-      })
-    ]);
-
-    console.log(`SEEDED: ${users.length} users`);
+    return queryInterface.bulkInsert(
+      "users",
+      [
+        {
+          name: "testuser",
+          email: "test@test.com",
+          password: bcrypt.hashSync("test1234", SALT_ROUNDS),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "dummy",
+          email: "dummy@dummy.com",
+          password: bcrypt.hashSync("dummy1234", SALT_ROUNDS),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("users", null, {});
-  }
+  },
 };
