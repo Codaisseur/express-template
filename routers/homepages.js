89291f11b0ba9aa3dce7576a61352b2d2821f720
Matias Garcia <matiasigarcia91@gmail.com>
2020-06-02 17:35:56 +0200

    update mypage

--- routers/homepages.js
+++ routers/homepages.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const Homepage = require("../models").homepage;
+const authMiddleware = require("../auth/middleware");
 
 const router = new Router();
 
@@ -11,5 +12,24 @@ router.get("/", async (req, res, next) => {
     next(e);
   }
 }); // /homepages/ - GET
+
+router.patch("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { title, description, color, backgroundColor } = req.body;
+    const { id } = req.params;
+    const toUpdate = await Homepage.findByPk(id);
+
+    const updated = await toUpdate.update({
+      title,
+      description,
+      color,
+      backgroundColor,
+    });
+
+    res.json(updated);
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
